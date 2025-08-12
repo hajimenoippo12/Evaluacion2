67a677811332287ac3ddaac07083d007222d39d7
cristopher.cornejo.valdivia@gmail.com <changeme@example.com>
2025-08-12 13:56:12 +0000

    Docker y CI/CD

---
+++ script.sh
@@ -0,0 +1,13 @@
+echo "FROM ubuntu:22.04" > Dockerfile
+   echo "RUN apt-get update && \
+   apt-get install -y --no-install-recommends apache2 && \
+   apt-get clean && \
+   rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*" >> Dockerfile
+   
+   echo "COPY index.html /var/www/html/" >> Dockerfile
+   echo "EXPOSE 80 " >> Dockerfile
+   echo 'CMD ["apache2ctl", "-D", "FOREGROUND"]' >> Dockerfile
+  
+  docker build -t web2 .
+  docker run -t -d -p 80:80 --name web2 web2
+  docker ps -a
